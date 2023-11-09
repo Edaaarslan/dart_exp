1afec98b5454d72600e8dc7ddfc09a981fdd3ef1
Edaaarslan <98347280+Edaaarslan@users.noreply.github.com>
2023-11-09 23:47:09 +0300

    Create class3.dart

---
+++ class3.dart
@@ -0,0 +1,18 @@
+class Animal {
+  int id;
+  String name;
+  String color;
+
+  Animal(this.id, this.name, this.color);
+}
+
+class Cat extends Animal {
+  String sound;
+
+  Cat(int id, String name, String color, this.sound) : super(id, name, color);
+}
+
+void main() {
+  Cat myCat = Cat(1, 'Whiskers', 'Gray', 'Meow');
+  print('ID: ${myCat.id}, Name: ${myCat.name}, Color: ${myCat.color}, Sound: ${myCat.sound}');
+}
